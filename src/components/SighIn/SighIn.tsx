8cb9a94438290cd8068e3f8a745788863ee438a8
Mari Vaypan <avemari98@gmail.com>
2023-02-17 02:18:17 +0300

    fix name

--- src/components/SighIn/SighIn.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export const SighIn: React.FC = () => {
-  return (
-    <div>SighIn</div>
-  )
-}
-
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import {Form} from './../Form/Form'
+
+export const SignIn: React.FC = () => {
+  return (
+    <section><Form title='signin'/></section>
+  )
+}
+
