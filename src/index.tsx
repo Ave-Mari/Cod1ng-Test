39c4c6a14e1f658baf8ccfd75cb10bd8cf5798a9
Mari Vaypan <avemari98@gmail.com>
2023-02-17 03:00:43 +0300

    move browser router to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import './main.css';
 import './main.scss';
 
 ReactDOM.render( 
-        <App />,
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,
       document.getElementById("app"), 
     
   );
